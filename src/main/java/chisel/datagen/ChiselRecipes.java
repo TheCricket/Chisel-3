36816e8e6e55af16708465d59d762c5b9fc2eca5
TheCricket <bcord@me.com>
2026-03-17 13:37:50 -0400

    Add missing recipes

--- src/main/java/chisel/datagen/ChiselRecipes.java
+++ src/main/java/chisel/datagen/ChiselRecipes.java
@@ -2,11 +2,15 @@ package chisel.datagen;
 
 import chisel.Chisel;
 import chisel.registry.ChiselItems;
+import chisel.registry.blocks.ChiselMetalBlocks;
+import chisel.registry.blocks.ChiselOtherBlocks;
+import chisel.registry.blocks.ChiselStoneBlocks;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStackTemplate;
@@ -26,6 +30,77 @@ public class ChiselRecipes extends RecipeProvider {
 
     @Override
     protected void buildRecipes() {
+        // MARK: Blocks
+        stoneAround("has_glowstone", Tags.Items.DUSTS_GLOWSTONE, ChiselOtherBlocks.ANTIBLOCK_BLACK, 8);
+        stoneAround("has_redstone", Tags.Items.DUSTS_REDSTONE, ChiselMetalBlocks.FUTURA_SCREEN_GRAY, 8);
+        stoneAround("has_quartz", Tags.Items.GEMS_QUARTZ, ChiselMetalBlocks.LABORATORY_FLOOR, 8);
+        stoneAround("has_iron_ingot", Tags.Items.INGOTS_IRON, ChiselMetalBlocks.TYRIAN_SHINY, 32);
+        stoneAround("has_pink_dye", Tags.Items.DYES_PINK, ChiselOtherBlocks.VALENTINES_BLOCK, 4);
+        stoneAround("has_coal", ItemTags.COALS, ChiselStoneBlocks.GRIMSTONE_RAW, 8);
+        stoneAround("has_coal_block", Tags.Items.STORAGE_BLOCKS_COAL, ChiselMetalBlocks.HEX_PLATING_0, 8);
+        stoneAround("has_feather", Tags.Items.FEATHERS, ChiselStoneBlocks.HOLYSTONE_RAW, 8);
+        stoneAround("has_lava_bucket", Tags.Items.BUCKETS_LAVA, ChiselStoneBlocks.LAVASTONE_RAW, 8);
+        stoneAround("has_water_bucket", Tags.Items.BUCKETS_WATER, ChiselStoneBlocks.WATERSTONE_BRICKS, 8);
+        stoneAround("has_blue_dye", Tags.Items.DYES_BLUE, ChiselStoneBlocks.TEMPLE_COBBLE, 8);
+        stoneAround("has_sign", ItemTags.SIGNS, ChiselMetalBlocks.WARNING_RADIATION, 4);
+
+        shaped(RecipeCategory.MISC, new ItemStackTemplate(ChiselMetalBlocks.FACTORY_DOTS.asItem(), 32))
+                .pattern("SIS")
+                .pattern("I I")
+                .pattern("SIS")
+                .define('S', Tags.Items.STONES)
+                .define('I', Tags.Items.INGOTS_IRON)
+                .unlockedBy("has_iron_ingots", has(Tags.Items.INGOTS_IRON))
+                .save(output);
+
+        shaped(RecipeCategory.MISC, new ItemStackTemplate(ChiselOtherBlocks.PAPERWALL_BOXED.asItem(), 8))
+                .pattern("PPP")
+                .pattern("PSP")
+                .pattern("PPP")
+                .define('P', Items.PAPER)
+                .define('S', Tags.Items.RODS_WOODEN)
+                .unlockedBy("has_paper", has(Items.PAPER))
+                .save(output);
+
+        shaped(RecipeCategory.MISC, new ItemStackTemplate(ChiselOtherBlocks.ROAD_LINES_WHITE.asItem(), 8))
+                .pattern("WRW")
+                .pattern("WRW")
+                .pattern("WRW")
+                .define('W', Tags.Items.DYES_WHITE)
+                .define('R', Tags.Items.DUSTS_REDSTONE)
+                .unlockedBy("has_white_dye", has(Tags.Items.DYES_WHITE))
+                .save(output);
+
+        shaped(RecipeCategory.MISC, new ItemStackTemplate(ChiselMetalBlocks.TECHNICAL_SCAFFOLD.asItem(), 8))
+                .pattern("SIS")
+                .pattern("ISI")
+                .pattern("SIS")
+                .define('S', Tags.Items.STONES)
+                .define('I', Tags.Items.INGOTS_IRON)
+                .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
+                .save(output);
+
+        shaped(RecipeCategory.MISC, new ItemStackTemplate(ChiselStoneBlocks.VOIDSTONE_RAW.asItem(), 8))
+                .pattern("OSO")
+                .pattern("SES")
+                .pattern("OSO")
+                .define('S', Tags.Items.STONES)
+                .define('E', Items.ENDER_PEARL)
+                .define('O', Tags.Items.OBSIDIANS)
+                .unlockedBy("has_ender_pearl", has(Items.ENDER_PEARL))
+                .save(output);
+
+        shaped(RecipeCategory.MISC, new ItemStackTemplate(ChiselStoneBlocks.ENERGISED_VOIDSTONE_RAW.asItem(), 8))
+                .pattern("OSO")
+                .pattern("SES")
+                .pattern("OSO")
+                .define('S', Tags.Items.STONES)
+                .define('E', Items.ENDER_EYE)
+                .define('O', Tags.Items.OBSIDIANS)
+                .unlockedBy("has_ender_eye", has(Items.ENDER_EYE))
+                .save(output);
+
+        // MARK: Items
         shaped(RecipeCategory.MISC, ChiselItems.CLOUD_IN_A_BOTTLE)
                 .pattern("G G")
                 .pattern("GQG")
@@ -58,6 +133,17 @@ public class ChiselRecipes extends RecipeProvider {
                 .requires(Items.GLASS_BOTTLE)
                 .requires(Items.STONE_SHOVEL)
                 .unlockedBy("has_glass_bottle", has(Items.GLASS_BOTTLE));
+    }
+
+    private void stoneAround(String unlockedBy, TagKey<Item> recipeItem, ItemLike outputItem, int count) {
+        shaped(RecipeCategory.MISC, new ItemStackTemplate(outputItem.asItem(), count))
+                .pattern("SSS")
+                .pattern("SRS")
+                .pattern("SSS")
+                .define('S', Tags.Items.STONES)
+                .define('R', recipeItem)
+                .unlockedBy(unlockedBy, has(recipeItem))
+                .save(output);
     }
 
     private void upgradeRecipe(String unlockedBy, ItemLike recipeItem, ItemLike upgradeItem) {
