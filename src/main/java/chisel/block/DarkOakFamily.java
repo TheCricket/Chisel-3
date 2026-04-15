1290a9677055bb8b93cfc1cb7703aa1bd1065c00
TheCricket <bcord@me.com>
2026-04-14 20:50:53 -0400

    Fix Dark Oak Planks textures

--- src/main/java/chisel/block/DarkOakFamily.java
+++ src/main/java/chisel/block/DarkOakFamily.java
@@ -10,16 +10,18 @@ public class DarkOakFamily extends ChiselBlock {
     public DarkOakFamily(BlockBehaviour.Properties props) {
         family = builder("dark_oak_planks")
                 .addVariant(Blocks.DARK_OAK_PLANKS)
-                .addVariant("dark_oak_planks_braced", props)
+                .addVariant("dark_oak_planks_braced", props, VariantModelType.CONNECTED_TBS)
                 .addVariant("dark_oak_planks_braid", props, VariantModelType.CONNECTED)
                 .addVariant("dark_oak_planks_crude_horizontal", props)
-                .addVariant("dark_oak_planks_crude_paneling", props)
-                .addVariant("dark_oak_planks_crude_vertical", props)
+                .addVariant("dark_oak_planks_crude_paneling", props, VariantModelType.MULTIBLOCK_3X3)
+                .addVariant("dark_oak_planks_crude_vertical", props, VariantModelType.MULTIBLOCK_3X3)
                 .addVariant("dark_oak_planks_encased", props, VariantModelType.CONNECTED)
                 .addVariant("dark_oak_planks_encased_large", props, VariantModelType.CONNECTED)
                 .addVariant("dark_oak_planks_encased_smooth", props, VariantModelType.CONNECTED)
                 .addVariant("dark_oak_planks_large", props)
-                .addVariant("dark_oak_planks_log_cabin", props)
+                .addVariant("dark_oak_planks_log_bordered", props, VariantModelType.CONNECTED)
+                .addVariant("dark_oak_planks_log_cabin_ns", props, VariantModelType.CONNECTED)
+                .addVariant("dark_oak_planks_log_cabin_ew", props, VariantModelType.CONNECTED)
                 .addVariant("dark_oak_planks_paneling", props, VariantModelType.CONNECTED)
                 .addVariant("dark_oak_planks_shipping", props, VariantModelType.CONNECTED)
                 .addVariant("dark_oak_planks_smooth", props, VariantModelType.CONNECTED)
@@ -39,7 +41,9 @@ public class DarkOakFamily extends ChiselBlock {
         lang.addBlock(getVariant("dark_oak_planks_encased_large"), "Dark Oak Wood Planks", "Encased Large Planks");
         lang.addBlock(getVariant("dark_oak_planks_encased_smooth"), "Dark Oak Wood Planks", "Encased Smooth");
         lang.addBlock(getVariant("dark_oak_planks_large"), "Dark Oak Wood Planks", "Large Planks");
-        lang.addBlock(getVariant("dark_oak_planks_log_cabin"), "Dark Oak Wood Planks", "Log Cabin");
+        lang.addBlock(getVariant("dark_oak_planks_log_bordered"), "Dark Oak Wood Planks", "Bordered");
+        lang.addBlock(getVariant("dark_oak_planks_log_cabin_ns"), "Dark Oak Wood Planks", "Log Cabin (NS)");
+        lang.addBlock(getVariant("dark_oak_planks_log_cabin_ew"), "Dark Oak Wood Planks", "Log Cabin (EW)");
         lang.addBlock(getVariant("dark_oak_planks_paneling"), "Dark Oak Wood Planks", "Paneling");
         lang.addBlock(getVariant("dark_oak_planks_shipping"), "Dark Oak Wood Planks", "Shipping Crate");
         lang.addBlock(getVariant("dark_oak_planks_smooth"), "Dark Oak Wood Planks", "Smooth");
